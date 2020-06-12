574b895cb3f25894b2b85d69b08f536cce7a5231
George, B. Smith <georgebsmith.tech@gmail.com>
2020-06-12 11:10:17 -0700

    added the grid view of the blog page and some default styles

--- server.js
+++ server.js
@@ -12,6 +12,33 @@ app.get("/", (req, res) => {
     res.render("home", { title: "Home" })
 })
 
+// const posts = [6, 8, 3, 7]
+const posts = [
+    {
+        image: "images/post1.png",
+        image_descr: "image of patients",
+        category: "Travel",
+        title: "Making sense of covid-19",
+        date: "23/34/13",
+        body: "it is a scam"
+
+    },
+    {
+        image: "images/post2.png",
+        image_descr: "image of patients",
+        category: "Fashion and design",
+        title: "The travelling sales man",
+        date: "12-june-2020",
+        body: "This is the day that i have been waiting for"
+
+    }
+]
+
+app.get("/blog", (req, res) => {
+
+    res.render("blog_home", { title: "BLOG", posts, cat_color: "fg-red" })
+})
+
 
 
 const PORT = process.env.PORT || 4000
