ee2211cda2f4f94d729ab15f852f11d41bfacb16
George, B. Smith <georgebsmith.tech@gmail.com>
2020-06-12 23:44:53 -0700

    modified the main blog section , with images

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get("/", (req, res) => {
 // const posts = [6, 8, 3, 7]
 const posts = [
     {
+        id:"12",
         image: "images/post1.png",
         image_descr: "image of patients",
         category: "Travel",
@@ -24,12 +25,33 @@ const posts = [
 
     },
     {
+        id:"2",
         image: "images/post2.png",
         image_descr: "image of patients",
         category: "Fashion and design",
         title: "The travelling sales man",
         date: "12-june-2020",
         body: "This is the day that i have been waiting for"
+
+    },
+    {
+        id:"1",
+        image: "images/post1.png",
+        image_descr: "image of patients",
+        category: "food and lifestyle",
+        title: "The power of 1hr",
+        date: "12-feb-2020",
+        body: "Working out in the morning is so reviving. You feel this much energy that spikes up your day..."
+
+    },
+        {
+        id:"9",
+        image: "images/post1.png",
+        image_descr: "image of patients",
+        category: "programming",
+        title: "Going boom with java",
+        date: "12-feb-2020",
+        body: "Working out in the morning is so reviving. You feel this much energy that spikes up your day..."
 
     }
 ]
@@ -38,6 +60,20 @@ app.get("/blog", (req, res) => {
 
     res.render("blog_home", { title: "BLOG", posts, cat_color: "fg-red" })
 })
+
+app.get("/blog/:id", (req, res) => {
+    let myPost=posts[0];
+    // posts.forEach(post => {
+    //     if(post.id===req.params.id){
+    //         myPost=post;
+    //     }
+    // });
+    console.log(myPost)
+    console.log(req.url)
+    res.render("blog_detailed", { title: `${myPost.category}|${myPost.title}`, myPost})
+})
+
+
 
 
 
