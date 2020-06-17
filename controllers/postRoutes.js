e7e3b179976a5fd6b4afe6fc88f7e1cb521d9e2a
George, B. Smith <georgebsmith.tech@gmail.com>
2020-06-17 12:06:59 -0700

    added a file field to admin bro

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -4,18 +4,12 @@ const router = express.Router()
 const PostModel = require("../models/postModel")
 
 
-const posts = [
 
-]
 router.get("/blog", (req, res) => {
     PostModel.find()
         .then(data => {
-            for (let post of data) {
-                post.image = ["images/post1.png", "images/post2.png"][Math.floor(Math.random() * 2)]
-                posts.push(post)
-            }
             // console.log(osts)
-            res.render("blog_home", { title: "BLOG", posts, cat_color: "fg-red", show: true })
+            res.render("blog_home", { title: "BLOG", posts: data, cat_color: "fg-red", show: true })
         })
         .catch(err => {
             console.log(err)
@@ -27,17 +21,15 @@ router.get("/blog", (req, res) => {
 })
 
 router.get("/blog/:id", (req, res) => {
-    let myPost = posts[0];
-    posts.forEach(post => {
-        if (post.id === req.params.id) {
-            myPost = post;
-        }
-    });
-    console.log(myPost)
-    // console.log(postBody)
-    console.log(myPost.markedBody)
-    console.log(req.url)
-    res.render("blog_detailed", { title: `${myPost.category}|${myPost.title}`, myPost, show: true })
+
+    PostModel.findOne({ _id: req.params.id })
+        .then((data) => {
+            console.log(data)
+            // res.end()
+            res.render("blog_detailed", { title: `${data.category}|${data.title}`, myPost: data, show: true })
+        })
+
+
 })
 
 router.post("/admin/posts/add", (req, res) => {
