c5a33f4818bafd2d491f884afd0a95e7d1d40ac0
George, B. Smith <georgebsmith.tech@gmail.com>
2020-06-25 15:16:51 -0700

    added a socail media share buttons

---
+++ public/js/social-media-share.js
@@ -0,0 +1,36 @@
+
+// https://wa.me/?text=[post-title] [post-url]
+
+
+// https://www.facebook.com/sharer.php?u=[post-url]
+
+// https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
+
+
+
+// https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+
+
+const twitterControl = document.querySelector(".social-media-share #twitter");
+const facebookControl = document.querySelector(".social-media-share #facebook");
+const whatsappControl = document.querySelector(".social-media-share #whatsapp");
+const linkedinControl = document.querySelector(".social-media-share #linkedin");
+
+
+function init() {
+    const blogUrl = encodeURI(document.location.href)
+    const postTitle = document.querySelector("#post-title").textContent;
+    const postImage = document.querySelector(".image#post-image").getAttribute("src")
+
+    facebookControl.setAttribute("href", `https://www.facebook.com/sharer.php?u=${blogUrl}`)
+    whatsappControl.setAttribute("href", `https://wa.me/?text=${postTitle} ${blogUrl}`)
+    linkedinControl.setAttribute("href", `https://www.linkedin.com/shareArticle?url=${blogUrl}&title=${postTitle}`)
+    twitterControl.setAttribute("href", `https://twitter.com/share?url=${blogUrl}&text=${postTitle}`)
+    // https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
+
+}
+
+init()
+
+
+
