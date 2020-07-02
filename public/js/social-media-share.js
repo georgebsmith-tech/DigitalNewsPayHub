145808e6426cca06c08f7c011a4a92c8b49ec048
George, B. Smith <georgebsmith.tech@gmail.com>
2020-07-02 10:16:56 -0700

    fixed a bug with content sharing

--- public/js/social-media-share.js
+++ public/js/social-media-share.js
@@ -18,15 +18,16 @@ const linkedinControl = document.querySelector(".social-media-share #linkedin");
 
 
 function init() {
+    alert("aamom")
     const blogUrl = encodeURI(document.location.href)
     const postTitle = document.querySelector("#post-title").textContent;
     const postImage = document.querySelector(".image#post-image").getAttribute("src")
 
-    facebookControl.setAttribute("href", `https://www.facebook.com/sharer.php?u=${blogUrl}`)
+    facebookControl.href = `https://www.facebook.com/sharer.php?u=${blogUrl}`
     whatsappControl.setAttribute("href", `https://wa.me/?text=${postTitle} ${blogUrl}`)
     linkedinControl.setAttribute("href", `https://www.linkedin.com/shareArticle?url=${blogUrl}&title=${postTitle}`)
     twitterControl.setAttribute("href", `https://twitter.com/share?url=${blogUrl}&text=${postTitle}`)
-    // https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
+    console.log(blogUrl)
 
 }
 
