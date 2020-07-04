757a19b9392383922877c993b9405cb6aafc7df2
George, B. Smith <georgebsmith.tech@gmail.com>
2020-07-04 01:00:36 -0700

    added the breaking news sections to the index page

--- public/js/like-post.js
+++ public/js/like-post.js
@@ -2,6 +2,7 @@ const likeHandler = document.querySelector(".like-post")
 
 
 likeHandler.addEventListener("click", async function () {
+    gsap.fromTo(".like-post", { scale: 2, duration: 1.5, backgroundColor: "red" }, { scale: 1 })
     const likeUrl = this.getAttribute("data-like-url")
     const resp = await fetch(likeUrl)
     const data = await resp.json()
